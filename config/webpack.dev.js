cb45e8e27bf81a2b88b0d176b5e051213efd84eb
Bryce Meyer <bryce13950@gmail.com>
2024-01-18 01:40:48 +0100

    fixed dev script

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -2,10 +2,14 @@ const path = require('path');
 
 const {merge} = require('webpack-merge');
 const common = require('./webpack.common.js');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 
 const dev = {
     mode: 'development',
+    plugins: [
+        new MiniCssExtractPlugin(),
+    ],
     devServer: {
         hot: "only",
         client: {
